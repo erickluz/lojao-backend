ab606fc6cfccf694dd53fc5da677e6756da5a053
Erick Luz <erickluz360@gmail.com>
2019-02-09 21:03:56 -0200

    Novos metodos http para Cliente

--- src/main/java/com/cia/lojao/resources/ClienteResource.java
+++ src/main/java/com/cia/lojao/resources/ClienteResource.java
@@ -1,15 +1,27 @@
 package com.cia.lojao.resources;
 
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cia.lojao.domain.Cliente;
+import com.cia.lojao.dto.ClienteDTO;
 import com.cia.lojao.services.ClienteService;
 
 @RestController
@@ -20,14 +32,51 @@ public class ClienteResource {
 	ClienteService service;
 
 	@GetMapping
-	public List<Cliente> listar() {
-		return service.listar();
+	public ResponseEntity<List<ClienteDTO>> listar() {
+		List<Cliente> cliente = service.listar();
+		List<ClienteDTO> clienteDTO = cliente.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(clienteDTO);
 	}
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<?> buscar(@PathVariable Integer id) {
+	public ResponseEntity<Cliente> buscar(@PathVariable Integer id) {
 		Cliente obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
 
+
+	@PostMapping
+	public ResponseEntity<Void> inserir(@Valid @RequestBody Cliente obj){
+		//Cliente obj = new Cliente(objDTO.getId(), objDTO.getNome());
+		service.inserir(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> alterar(@Valid @RequestBody ClienteDTO obj, @PathVariable Integer id){
+		Cliente cliente = new Cliente(obj.getId(), obj.getNome(), obj.getEmail(), null, null);
+		cliente.setId(id);
+		cliente = service.atualizar(cliente);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<?> excluir(@PathVariable Integer id){
+		service.excluir(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/page")
+	public ResponseEntity<Page<ClienteDTO>> listarPagina(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Cliente> lista = service.listarPagina(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listaDTO = lista.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listaDTO);
+	}
+	
+	
 }
--- src/main/java/com/cia/lojao/services/ClienteService.java
+++ src/main/java/com/cia/lojao/services/ClienteService.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cia.lojao.domain.Cliente;
 import com.cia.lojao.repositories.ClienteRepository;
+import com.cia.lojao.services.exceptions.DataIntegrityException;
 import com.cia.lojao.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -25,5 +30,33 @@ public class ClienteService {
 	public List<Cliente> listar() {;
 		return repo.findAll();
 	}
+	
+	public Cliente inserir(Cliente cliente) {
+		cliente.setId(null);
+		return repo.save(cliente);
+	}
+	
+	public Cliente atualizar(Cliente cliente) {
+		Cliente obj = buscar(cliente.getId());
+		cliente.setCpfOuCnpj(obj.getCpfOuCnpj());
+		cliente.setTipo(obj.getTipo());
+		return repo.save(cliente);
+	}
+	
+	public void excluir(Integer id) {
+		buscar(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao e possivel excluir um cliente porque ha entidades relacionadas", e);
+		}
+	}
+	
+	public Page<Cliente> listarPagina(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	
 	
 }

c6fc6b8c91788eac90ab2580c7a017d4c27e4658
Erick <erickluz360@gmail.com>
2019-03-04 01:53:03 -0300

    Configuracao inicial Spring Security

---
+++ src/main/java/com/cia/lojao/config/SecurityConfig.java
@@ -0,0 +1,76 @@
+package com.cia.lojao.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+	
+	/*
+	 * Definicao das URLs permitidas
+	 * URLs dentro do vetor PUBLIC_MATCHERS serao permitidas o resto nao !.
+	 * 
+	 */
+	
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**",
+	};	
+	
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/produtos/**",
+			"/categorias/**"
+	};
+	
+	
+	/*
+	 * Sobrescrevendo a configuracao padrao
+	 * Configuracao para permitir somente as URLs contidas em PUBLIC_MATCHERS
+	 */
+	
+	@Override
+	protected void configure (HttpSecurity http) throws Exception {
+		
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+			http.headers().frameOptions().disable(); // Liberar H2 caso esteja no perfil de testes
+		}
+		
+		http.cors().and().csrf().disable(); // Desabilita protecao automatica contra ataque CSRF
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+		.antMatchers(PUBLIC_MATCHERS).permitAll()
+		.anyRequest()
+		.authenticated();
+		// Garantir que a aplicacao e Stateless e nao trabalhara com sessao.
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	/*
+	 * Permissao de acesso de multiplas fontes CORS
+	 */
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+	    return source;
+	}
+
+	
+	
+}
